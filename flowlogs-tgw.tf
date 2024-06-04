eff8739f49a7efa35e8ce93e58494740d0fc6126
Cristian Beraha <cristian@cloudops.works>
2024-06-04 20:48:16 -0300

    fix: max_aggregation_interval = 60 default not working

--- flowlogs-tgw.tf
+++ flowlogs-tgw.tf
@@ -4,11 +4,12 @@
 #            Distributed Under Apache v2.0 License
 #
 resource "aws_flow_log" "tgw_flow_logs" {
-  count              = var.transit_gateway.enabled ? 1 : 0
-  iam_role_arn       = aws_iam_role.vpc_logs.arn
-  log_destination    = aws_cloudwatch_log_group.tgw_log_group[0].arn
-  traffic_type       = var.flow_logs_type
-  transit_gateway_id = module.transit_gateway[0].ec2_transit_gateway_id
+  count                    = var.transit_gateway.enabled ? 1 : 0
+  iam_role_arn             = aws_iam_role.vpc_logs.arn
+  log_destination          = aws_cloudwatch_log_group.tgw_log_group[0].arn
+  max_aggregation_interval = 60
+  traffic_type             = var.flow_logs_type
+  transit_gateway_id       = module.transit_gateway[0].ec2_transit_gateway_id
 }
 
 resource "aws_cloudwatch_log_group" "tgw_log_group" {
@@ -22,6 +23,7 @@ resource "aws_flow_log" "tgw_att_flow_logs" {
   iam_role_arn                  = aws_iam_role.vpc_logs.arn
   log_destination               = aws_cloudwatch_log_group.tgw_att_log_group[0].arn
   traffic_type                  = var.flow_logs_type
+  max_aggregation_interval      = 60
   transit_gateway_attachment_id = module.transit_gateway[0].ec2_transit_gateway_vpc_attachment_ids[count.index]
 }
 
