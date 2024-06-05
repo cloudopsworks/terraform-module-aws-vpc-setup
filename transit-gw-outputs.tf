7283520213874b842848498563c43555666970f5
Cristian Beraha <cristian@cloudops.works>
2024-06-04 21:37:24 -0300

    fix: outputs

--- transit-gw-outputs.tf
+++ transit-gw-outputs.tf
@@ -4,14 +4,14 @@
 #            Distributed Under Apache v2.0 License
 #
 locals {
-  tgw_output = var.transit_gateway.enabled ? {
+  tgw_output = (var.transit_gateway.enabled ? {
     id              = module.transit_gateway[0].ec2_transit_gateway_id
     arn             = module.transit_gateway[0].ec2_transit_gateway_arn
     route_table_id  = module.transit_gateway[0].ec2_transit_gateway_vpc_attachment_ids
     route_ids       = module.transit_gateway[0].ec2_transit_gateway_route_ids
     route_table_ids = module.transit_gateway[0].ec2_transit_gateway_route_table_association_ids
     vpc_attachments = module.transit_gateway[0].ec2_transit_gateway_vpc_attachment_ids
-  } : {}
+  } : {})
 }
 
 output "transit_gateway" {
