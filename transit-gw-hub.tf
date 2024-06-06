012b2e82d2f913127dbf354cc7f5b38425c42a7f
Cristian Beraha <cristian@cloudops.works>
2024-06-05 21:07:25 -0300

    fix: creation logic

--- transit-gw-hub.tf
+++ transit-gw-hub.tf
@@ -14,18 +14,32 @@ locals {
       tgw_routes                                      = var.transit_gateway.routes
     }
   } : {}
-  spokes = {}
-  vpcs   = merge(local.hubs, local.spokes)
+  spokes = var.transit_gateway.enabled && var.is_hub ? {
+    spoke = {
+      vpc_id                                          = module.vpc.vpc_id
+      subnet_ids                                      = module.vpc.private_subnets
+      dns_support                                     = true
+      transit_gateway_default_route_table_association = false
+      transit_gateway_default_route_table_propagation = false
+      tgw_routes                                      = var.transit_gateway.routes
+    }
+  } : {}
+  vpcs = merge(local.hubs, local.spokes)
 }
 
 module "transit_gateway" {
-  count           = var.transit_gateway.enabled ? 1 : 0
-  source          = "terraform-aws-modules/transit-gateway/aws"
-  version         = "~> 2.10"
-  name            = "tgw-${local.system_name}"
-  description     = "Transit Gateway for Hub ${var.spoke_def}"
-  create_tgw      = var.is_hub
-  share_tgw       = !var.is_hub
-  vpc_attachments = local.vpcs
-  tags            = local.all_tags
+  count                                 = var.transit_gateway.enabled ? 1 : 0
+  source                                = "terraform-aws-modules/transit-gateway/aws"
+  version                               = "~> 2.10"
+  name                                  = "tgw-${local.system_name}"
+  description                           = "Transit Gateway for Hub ${var.spoke_def}"
+  create_tgw                            = var.is_hub
+  share_tgw                             = !var.is_hub
+  vpc_attachments                       = local.vpcs
+  ram_allow_external_principals         = tr(var.transit_gateway.ram.allow_external_principals, false)
+  ram_principals                        = tr(var.transit_gateway.ram.principals, [])
+  enable_auto_accept_shared_attachments = tr(var.transit_gateway.enable_auto_accept, false)
+
+  ram_resource_share_arn = try(var.transit_gateway.ram_share_id, null)
+  tags                   = var.extra_tags
 }
--- transit-gw-outputs.tf
+++ transit-gw-outputs.tf
@@ -4,13 +4,31 @@
 #            Distributed Under Apache v2.0 License
 #
 locals {
-  tgw_output = (var.transit_gateway.enabled ? {
+  tgw_output_hub = (var.transit_gateway.enabled && var.is_hub ? {
     id              = module.transit_gateway[0].ec2_transit_gateway_id
     arn             = module.transit_gateway[0].ec2_transit_gateway_arn
     route_table_id  = module.transit_gateway[0].ec2_transit_gateway_vpc_attachment_ids
     route_ids       = module.transit_gateway[0].ec2_transit_gateway_route_ids
     route_table_ids = module.transit_gateway[0].ec2_transit_gateway_route_table_association_ids
     vpc_attachments = module.transit_gateway[0].ec2_transit_gateway_vpc_attachment_ids
+    ram_share_id    = module.transit_gateway[0].ram_resource_share_id
+    } : {
+    id              = null
+    arn             = null
+    route_table_id  = null
+    route_ids       = null
+    route_table_ids = null
+    vpc_attachments = null
+
+  })
+  tgw_output_spoke = (var.transit_gateway.enabled && !var.is_hub ? {
+    id              = module.transit_gateway[0].ec2_transit_gateway_id
+    arn             = module.transit_gateway[0].ec2_transit_gateway_arn
+    route_table_id  = module.transit_gateway[0].ec2_transit_gateway_vpc_attachment_ids
+    route_ids       = module.transit_gateway[0].ec2_transit_gateway_route_ids
+    route_table_ids = module.transit_gateway[0].ec2_transit_gateway_route_table_association_ids
+    vpc_attachments = module.transit_gateway[0].ec2_transit_gateway_vpc_attachment_ids
+    ram_share_id    = module.transit_gateway[0].ram_resource_share_id
     } : {
     id              = null
     arn             = null
@@ -22,6 +40,10 @@ locals {
   })
 }
 
-output "transit_gateway" {
-  value = local.tgw_output
+output "hub_transit_gateway" {
+  value = local.tgw_output_hub
+}
+
+output "spoke_transit_gateway" {
+  value = local.tgw_output_spoke
 }
