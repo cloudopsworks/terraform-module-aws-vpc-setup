6e79574a23f0fc5257546c15d9a307cb34baaabe
Cristian Beraha <cristian@cloudops.works>
2024-06-06 12:47:20 -0300

    fix: flowlogs only on hub

--- flowlogs-tgw.tf
+++ flowlogs-tgw.tf
@@ -29,7 +29,7 @@ resource "aws_cloudwatch_log_group" "tgw_log_group" {
 # TGW Attachment
 resource "aws_flow_log" "tgw_att_flow_logs" {
   provider                      = aws.default
-  count                         = var.transit_gateway.enabled && var.is_hub ? length(module.transit_gateway[0].ec2_transit_gateway_vpc_attachment_ids) : 0
+  count                         = var.transit_gateway.enabled ? length(module.transit_gateway[0].ec2_transit_gateway_vpc_attachment_ids) : 0
   iam_role_arn                  = aws_iam_role.vpc_logs.arn
   log_destination               = aws_cloudwatch_log_group.tgw_att_log_group[0].arn
   traffic_type                  = var.flow_logs_type
@@ -46,6 +46,6 @@ resource "aws_flow_log" "tgw_att_flow_logs" {
 
 resource "aws_cloudwatch_log_group" "tgw_att_log_group" {
   provider = aws.default
-  count    = var.transit_gateway.enabled && var.is_hub ? 1 : 0
+  count    = var.transit_gateway.enabled ? 1 : 0
   name     = "network/${local.flowlogs_prefix}/${var.spoke_def}/tgw-att-${local.system_name}"
 }
