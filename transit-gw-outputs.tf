904214b08b233214c7a26d54f4015a1c6c4ee5bb
Cristian Beraha <cristian@cloudops.works>
2024-06-05 23:26:05 -0300

    feat: output of input settings

--- transit-gw-outputs.tf
+++ transit-gw-outputs.tf
@@ -5,24 +5,33 @@
 #
 locals {
   tgw_output_hub = (var.transit_gateway.enabled ? {
-    id              = module.transit_gateway[0].ec2_transit_gateway_id
-    arn             = module.transit_gateway[0].ec2_transit_gateway_arn
-    route_table_id  = module.transit_gateway[0].ec2_transit_gateway_vpc_attachment_ids
-    route_ids       = module.transit_gateway[0].ec2_transit_gateway_route_ids
-    route_table_ids = module.transit_gateway[0].ec2_transit_gateway_route_table_association_ids
-    vpc_attachments = module.transit_gateway[0].ec2_transit_gateway_vpc_attachment_ids
-    ram_share_id    = module.transit_gateway[0].ram_resource_share_id
+    id                          = module.transit_gateway[0].ec2_transit_gateway_id
+    arn                         = module.transit_gateway[0].ec2_transit_gateway_arn
+    route_table_ids             = module.transit_gateway[0].ec2_transit_gateway_vpc_attachment_ids
+    route_ids                   = module.transit_gateway[0].ec2_transit_gateway_route_ids
+    route_table_association_ids = module.transit_gateway[0].ec2_transit_gateway_route_table_association_ids
+    vpc_attachments             = module.transit_gateway[0].ec2_transit_gateway_vpc_attachment_ids
+    ram_share_id                = module.transit_gateway[0].ram_resource_share_id
     } : {
-    id              = null
-    arn             = null
-    route_table_id  = null
-    route_ids       = null
-    route_table_ids = null
-    vpc_attachments = null
-    ram_share_id    = null
+    id                          = null
+    arn                         = null
+    route_table_ids             = null
+    route_ids                   = null
+    route_table_association_ids = null
+    vpc_attachments             = null
+    ram_share_id                = null
   })
 }
 
 output "transit_gateway" {
   value = local.tgw_output_hub
+}
+
+output "transit_gateway_settings" {
+  value = {
+    is_hub     = var.is_hub
+    tgw        = var.transit_gateway
+    vpcs       = local.vpcs
+    shared_tgw = var.shared_transit_gateway
+  }
 }
