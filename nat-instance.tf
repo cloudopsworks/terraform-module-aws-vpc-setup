e086e71523b1c8c158dde10217cc5491a0dd8ea9
Cristian Beraha <cristian@cloudops.works>
2025-08-14 00:22:22 -0300

    feat: Add EIP and association resources to NAT instance configuration for enhanced flexibility

--- nat-instance.tf
+++ nat-instance.tf
@@ -18,8 +18,21 @@ data "aws_ami" "amazon_linux_2023" {
   }
 }
 
-resource "aws_network_interface" "nat_ec2_instance" {
+resource "aws_eip" "nat_ec2_instance" {
   count = local.use_nat_instance ? 1 : 0
+  tags = merge(local.all_tags, {
+    Name = "nat-instance-eip-${local.system_name}"
+  })
+}
+
+resource "aws_eip_association" "nat_ec2_instance" {
+  count                = local.use_nat_instance ? 1 : 0
+  allocation_id        = aws_eip.nat_ec2_instance[0].id
+  network_interface_id = aws_network_interface.nat_ec2_instance[0].id
+}
+
+resource "aws_network_interface" "nat_ec2_instance" {
+  count             = local.use_nat_instance ? 1 : 0
   security_groups   = [aws_security_group.ssh_admin.id, aws_security_group.bastion.id]
   subnet_id         = module.vpc.public_subnets[0] # Use the first public subnet for NAT
   source_dest_check = false
@@ -30,11 +43,11 @@ resource "aws_network_interface" "nat_ec2_instance" {
 }
 
 resource "aws_instance" "nat_ec2_instance" {
-  count                       = local.use_nat_instance ? 1 : 0
-  ami                         = data.aws_ami.amazon_linux_2023[0].id
-  instance_type               = var.nat_instance_size # Or other suitable type
-  iam_instance_profile        = aws_iam_instance_profile.bastion.name
-  user_data                   = <<-EOF
+  count                = local.use_nat_instance ? 1 : 0
+  ami                  = data.aws_ami.amazon_linux_2023[0].id
+  instance_type        = var.nat_instance_size # Or other suitable type
+  iam_instance_profile = aws_iam_instance_profile.bastion.name
+  user_data            = <<-EOF
     #!/bin/bash
     yum install iptables-services -y
     echo "net.ipv4.ip_forward=1" >> /etc/sysctl.d/90-nat.conf
