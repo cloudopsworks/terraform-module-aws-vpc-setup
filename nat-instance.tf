fb13ff25cd6fe9c9986779ed559a7e4a4d9028dc
Cristian Beraha <cristian@cloudops.works>
2025-08-13 23:46:36 -0300

    feat: Refactor NAT instance setup with Amazon Linux 2023 and modularize high-volume NAT configuration

--- nat-instance.tf
+++ nat-instance.tf
@@ -7,47 +7,43 @@
 #     Distributed Under Apache v2.0 License
 #
 
-data "aws_subnet" "alternat_subnet" {
-  count = length(module.vpc.private_subnets)
-  id    = module.vpc.private_subnets[count.index]
+data "aws_ami" "amazon_linux_2023" {
+  count       = local.use_nat_instance ? 1 : 0
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["al2023-ami-*-x86_64"] # Or arm64 for Graviton instances
+  }
 }
 
-module "alternat_instances" {
-  count               = var.high_volume_nat ? 1 : 0
-  source              = "chime/alternat/aws"
-  version             = "~> 0.9"
-  lambda_package_type = "Zip"
-  nat_instance_block_devices = {
-    xvda = {
-      device_name = "/dev/xvda"
-      ebs = {
-        encrypted   = true
-        volume_type = "gp3"
-        volume_size = 8
-      }
-    }
+resource "aws_instance" "nat_ec2_instance" {
+  count                  = local.use_nat_instance ? 1 : 0
+  ami                    = data.aws_ami.amazon_linux_2023[0].id
+  instance_type          = var.nat_instance_size        # Or other suitable type
+  subnet_id              = module.vpc.public_subnets[0] # Use the first public subnet for NAT
+  vpc_security_group_ids = [aws_security_group.ssh_admin.id, aws_security_group.bastion.id]
+  iam_instance_profile   = aws_iam_instance_profile.bastion.name
+  source_dest_check      = false # Essential for a NAT device
+  user_data              = <<-EOF
+    #!/bin/bash
+    yum install iptables-services -y
+    echo "net.ipv4.ip_forward=1" >> /etc/sysctl.d/90-nat.conf
+    sysctl --system
+    iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
+    iptables-save > /etc/sysconfig/iptables
+  EOF
+
+  root_block_device {
+    volume_size           = 8
+    delete_on_termination = true
   }
-  vpc_id                                = module.vpc.vpc_id
-  ingress_security_group_ids            = [aws_security_group.ssh_admin.id, aws_security_group.bastion.id]
-  nat_instance_iam_profile_name         = "nat-instance-${local.system_name}"
-  nat_instance_iam_role_name            = "nat-instance-${local.system_name}"
-  nat_instance_lifecycle_hook_role_name = "nat-lifecycle-hook-${local.system_name}"
-  nat_instance_name_prefix              = "nat-instance-${local.system_name_short}"
-  nat_instance_sg_name_prefix           = "nat-instance-${local.system_name_short}"
-  nat_lambda_function_role_name         = "nat-lambda-${local.system_name_short}"
-  nat_instance_type                     = var.nat_instance_size
-  tags                                  = local.all_tags
-  vpc_az_maps = [
-    for index, rt in module.vpc.private_route_table_ids : {
-      az               = data.aws_subnet.alternat_subnet[index].availability_zone
-      route_table_ids  = [rt]
-      public_subnet_id = module.vpc.public_subnets[index]
-      # The secondary subnets do not need to be included here. this data is
-      # used for the connectivity test function and VPC endpoint which are
-      # only needed in one subnet per zone.
-      private_subnet_ids = [module.vpc.private_subnets[index]]
-    }
-  ]
-  nat_instance_eip_ids = var.reuse_nat_ips ? var.external_nat_ip_ids : []
-  depends_on           = [module.vpc]
+
+  volume_tags = merge(local.all_tags, {
+    Name = "nat-instance-${local.system_name}"
+  })
+  tags = merge(local.all_tags, {
+    Name = "nat-instance-${local.system_name}"
+  })
 }
