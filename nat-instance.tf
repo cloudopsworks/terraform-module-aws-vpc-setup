cfb57bbee96c62bde3ba96ab8c9a05c37fc5f745
Cristian Beraha <cristian@cloudops.works>
2025-08-22 09:14:19 -0300

    feat: Update NAT instance module configuration and adjust .gitignore for additional build files

--- nat-instance.tf
+++ nat-instance.tf
@@ -8,13 +8,24 @@
 #
 
 module "nat_instance" {
-  count                       = local.use_nat_instance ? 1 : 0
-  source                      = "git::https://github.com/cloudopsworks/terraform-aws-nat-instance.git//?ref=v0.1.0-beta.5"
-  name                        = "nat-instance-${local.system_name}"
-  vpc_id                      = module.vpc.vpc_id
-  public_subnet               = element(module.vpc.public_subnets, 0)
-  private_subnets_cidr_blocks = concat(module.vpc.private_subnets_cidr_blocks, var.nat_instance_allowed_cidrs)
-  private_route_table_ids     = module.vpc.private_route_table_ids
+  count                = local.use_nat_instance ? 1 : 0
+  source               = "git::https://github.com/cloudopsworks/terraform-aws-fck-nat.git//?ref=main"
+  name                 = "nat-instance-${local.system_name}"
+  vpc_id               = module.vpc.vpc_id
+  subnet_id            = element(module.vpc.public_subnets, 0)
+  eip_allocation_ids   = [aws_eip.nat_ec2_instance[0].id]
+  use_cloudwatch_agent = true
+  update_route_tables  = true
+  instance_type        = var.nat_instance_size
+  route_table_ids = merge({
+    for rtb in module.vpc.private_route_table_ids : "private-${rtb}" => rtb
+    }, {
+    for rtb in module.vpc.intra_route_table_ids : "intra-${rtb}" => rtb
+    }, {
+    for rtb in module.vpc.database_route_table_ids : "db-${rtb}" => rtb
+    }
+  )
+  tags = local.all_tags
 }
 
 resource "aws_eip" "nat_ec2_instance" {
